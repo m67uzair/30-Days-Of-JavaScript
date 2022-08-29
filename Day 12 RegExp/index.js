f5b44418465961f2b28f923c41d5fa3916cd3da2
m67uzair <m67uzair@gmail.com>
2022-08-30 01:25:58 +0500

    test commit

--- Day 12 RegExp/index.js
+++ Day 12 RegExp/index.js
@@ -1,5 +1,16 @@
-const pattern = /\d/g  // d is a special character which means digits
-const txt = 'This regular expression example was made in January 12,  2020.'
-const matches = txt. match(pattern)
+let pattern = /\d+/g; // d is a special character which means digits
+let txt = "This regular expression example was made in January12a,  2020.";
+let matches = txt.match(pattern);
 
-console.log(matches)  // ["1", "2", "2", "0", "2", "0"], this is not what we want
+console.log(matches); // [12,2020]
+
+pattern = /[a].+/g; // . any character, + any character one or more times
+txt = "Apple and banana are fruits";
+matches = txt.match(pattern);
+
+console.log(matches); // ['and banana are fruits']
+
+txt = "This regular expression example was made in December 6,  2019";
+pattern = /\b[^0-9]\w{4}\b/g; //  exactly four character  words without numbers
+matches = txt.match(pattern);
+console.log(matches); //['This', 'made']
