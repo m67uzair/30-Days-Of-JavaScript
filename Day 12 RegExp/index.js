034a4d8e8d4439c4357869021dde4e4be47be812
m67uzair <m67uzair@gmail.com>
2022-08-31 15:17:21 +0500

    Day 12 Done

--- Day 12 RegExp/index.js
+++ Day 12 RegExp/index.js
@@ -61,7 +61,7 @@ matches = txt.match(pattern);
 
 matches = matches.map((number) => parseInt(number)).sort((a, b) => a - b);
 
-console.log(matches);
+// console.log(matches);
 
 // Write a pattern which identify if a string is a valid JavaScript variable
 
@@ -97,4 +97,17 @@ function mostFrequentWords(text, count) {
   }
 }
 let para =   "I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.";
+
 // mostFrequentWords(para,10);
+
+function cleanText(text='') {
+  let pattern = /[^\w+ .?]/g;
+  text = text.replace(pattern,'');
+
+  return text;
+}
+
+txt ='%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'
+
+// console.log( cleanText(txt) );
+
