51655b9b799b1a3053b119be7bb8150ee566614e
m67uzair <m67uzair@gmail.com>
2022-09-01 12:23:31 +0500

    DAy 13 start

---
+++ Day 13 Console Objects/index.js
@@ -0,0 +1,151 @@
+console.log("%c30 Days Of JavaScript", "color:green"); // log output is green
+console.log(
+  "%c30 Days%c %cOf%c %cJavaScript%c",
+  "color:green",
+  "",
+  "color:red",
+  "",
+  "color:yellow"
+); // log output green red and yellow text
+
+console.warn("This is a warning");
+console.warn(
+  "You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!"
+);
+console.warn("Warning is different from error");
+
+console.error("This is an error message");
+console.error("We all make mistakes");
+
+let names = ["Asabeneh", "Brook", "David", "John"];
+console.table(names);
+
+let user = {
+  name: "Asabeneh",
+  title: "Programmer",
+  country: "Finland",
+  city: "Helsinki",
+  age: 250,
+};
+console.table(user);
+
+let countries = [
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+console.table(countries);
+
+let users = [
+  {
+    name: "Asabeneh",
+    title: "Programmer",
+    country: "Finland",
+    city: "Helsinki",
+    age: 250,
+  },
+  {
+    name: "Eyob",
+    title: "Teacher",
+    country: "Sweden",
+    city: "London",
+    age: 25,
+  },
+  {
+    name: "Asab",
+    title: "Instructor",
+    country: "Norway",
+    city: "Oslo",
+    age: 22,
+  },
+  {
+    name: "Matias",
+    title: "Developer",
+    country: "Denmark",
+    city: "Copenhagen",
+    age: 28,
+  },
+];
+console.table(users);
+
+countries = [
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+
+console.time("Regular for loop");
+for (let i = 0; i < countries.length; i++) {
+  console.log(countries[i][0], countries[i][1]);
+}
+console.timeEnd("Regular for loop");
+
+console.time("for of loop");
+for (let [name, city] of countries) {
+  console.log(name, city);
+}
+console.timeEnd("for of loop");
+
+console.time("forEach loop");
+countries.forEach(([name, city]) => {
+  console.log(name, city);
+});
+console.timeEnd("forEach loop");
+console.clear();
+
+names = ["Asabeneh", "Brook", "David", "John"];
+countries = [
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+user = {
+  name: "Asabeneh",
+  title: "Programmer",
+  country: "Finland",
+  city: "Helsinki",
+  age: 250,
+};
+users = [
+  {
+    name: "Asabeneh",
+    title: "Programmer",
+    country: "Finland",
+    city: "Helsinki",
+    age: 250,
+  },
+  {
+    name: "Eyob",
+    title: "Teacher",
+    country: "Sweden",
+    city: "London",
+    age: 25,
+  },
+  {
+    name: "Asab",
+    title: "Instructor",
+    country: "Norway",
+    city: "Oslo",
+    age: 22,
+  },
+  {
+    name: "Matias",
+    title: "Developer",
+    country: "Denmark",
+    city: "Copenhagen",
+    age: 28,
+  },
+];
+
+console.group("Names");
+console.log(names);
+console.groupEnd();
+
+console.group("Countries");
+console.log(countries);
+console.groupEnd();
+
+console.group("Users");
+console.log(user);
+console.log(users);
+console.groupEnd();
