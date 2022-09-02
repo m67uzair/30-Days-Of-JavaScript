820f0e2f3dbcecf56ad649fc79037cf86b09cb4e
m67uzair <m67uzair@gmail.com>
2022-09-02 22:34:48 +0500

    Start Day 15 Excerices

--- Day 15 Classes/index.js
+++ Day 15 Classes/index.js
@@ -1,63 +1,118 @@
 class Person {
   constructor(firstName, lastName, age, country, city) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.country = country
-    this.city = city
-    this.score = 0
-    this.skills = []
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.country = country;
+    this.city = city;
+    this.score = 0;
+    this.skills = [];
   }
   getFullName() {
-    const fullName = this.firstName + ' ' + this.lastName
-    return fullName
+    const fullName = this.firstName + " " + this.lastName;
+    return fullName;
   }
   get getScore() {
-    return this.score
+    return this.score;
   }
   get getSkills() {
-    return this.skills
+    return this.skills;
   }
   set setScore(score) {
-    this.score += score
+    this.score += score;
   }
   set setSkill(skill) {
-    this.skills.push(skill)
+    this.skills.push(skill);
   }
   getPersonInfo() {
-    let fullName = this.getFullName()
+    let fullName = this.getFullName();
     let skills =
       this.skills.length > 0 &&
-      this.skills.slice(0, this.skills.length - 1).join(', ') +
-        ` and ${this.skills[this.skills.length - 1]}`
-    let formattedSkills = skills ? `He knows ${skills}` : ''
+      this.skills.slice(0, this.skills.length - 1).join(", ") +
+        ` and ${this.skills[this.skills.length - 1]}`;
+    let formattedSkills = skills ? `He knows ${skills}` : "";
 
-    let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
-    return info
+    let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`;
+    return info;
   }
 }
 
-const person1 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
-const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
-const person3 = new Person('John', 'Doe', 50, 'Mars', 'Mars city')
+const person1 = new Person("Asabeneh", "Yetayeh", 250, "Finland", "Helsinki");
+const person2 = new Person("Lidiya", "Tekle", 28, "Finland", "Espoo");
+const person3 = new Person("John", "Doe", 50, "Mars", "Mars city");
 
-person1.setScore = 1
-person1.setSkill = 'HTML'
-person1.setSkill = 'CSS'
-person1.setSkill = 'JavaScript'
+person1.setScore = 1;
+person1.setSkill = "HTML";
+person1.setSkill = "CSS";
+person1.setSkill = "JavaScript";
 
-person2.setScore = 1
-person2.setSkill = 'Planning'
-person2.setSkill = 'Managing'
-person2.setSkill = 'Organizing'
+person2.setScore = 1;
+person2.setSkill = "Planning";
+person2.setSkill = "Managing";
+person2.setSkill = "Organizing";
 
-console.log(person1.getScore)
-console.log(person2.getScore)
+console.log(person1.getScore);
+console.log(person2.getScore);
 
-console.log(person1.getSkills)
-console.log(person2.getSkills)
-console.log(person3.getSkills)
+console.log(person1.getSkills);
+console.log(person2.getSkills);
+console.log(person3.getSkills);
 
-console.log(person1.getPersonInfo())
-console.log(person2.getPersonInfo())
-console.log(person3.getPersonInfo())
+console.log(person1.getPersonInfo());
+console.log(person2.getPersonInfo());
+console.log(person3.getPersonInfo());
+
+console.clear();
+
+class Student extends Person {
+  constructor(firstName, lastName, age, country, city, gender) {
+    super(firstName, lastName, age, country, city);
+    this.gender = gender;
+  }
+
+  saySomething() {
+    console.log("I am a child of the person class");
+  }
+  getPersonInfo() {
+    let fullName = this.getFullName();
+    let skills =
+      this.skills.length > 0 &&
+      this.skills.slice(0, this.skills.length - 1).join(", ") +
+        ` and ${this.skills[this.skills.length - 1]}`;
+
+    let formattedSkills = skills ? `He knows ${skills}` : "";
+    let pronoun = this.gender == "Male" ? "He" : "She";
+
+    let info = `${fullName} is ${this.age}. ${pronoun} lives in ${this.city}, ${this.country}. ${formattedSkills}`;
+    return info;
+  }
+}
+
+const s1 = new Student(
+  "Asabeneh",
+  "Yetayeh",
+  250,
+  "Finland",
+  "Helsinki",
+  "Male"
+);
+const s2 = new Student("Lidiya", "Tekle", 28, "Finland", "Helsinki", "Female");
+s1.setScore = 1;
+s1.setSkill = "HTML";
+s1.setSkill = "CSS";
+s1.setSkill = "JavaScript";
+
+s2.setScore = 1;
+s2.setSkill = "Planning";
+s2.setSkill = "Managing";
+s2.setSkill = "Organizing";
+
+console.log(s1);
+
+s1.saySomething();
+console.log(s1.getFullName());
+console.log(s1.getPersonInfo());
+
+s2.saySomething();
+console.log(s2.getFullName());
+console.log(s2.getPersonInfo());
