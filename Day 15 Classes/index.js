cf884696cb368875b192b9cd562b951cd6a76d7f
m67uzair <m67uzair@gmail.com>
2022-09-03 13:59:32 +0500

    minor fix

--- Day 15 Classes/index.js
+++ Day 15 Classes/index.js
@@ -166,6 +166,8 @@ console.log(cat.getAnimalInfo());
 
 console.clear();
 
+// Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
 class PersonAccount{
   constructor(firstName, lastName, incomes,expenses){
     this.firstName = firstName;
