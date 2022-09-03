305b43d0c4304c5c37d421c58ad1dcf41675e92b
m67uzair <m67uzair@gmail.com>
2022-09-03 20:15:52 +0500

    Day 16 done

--- Day 16 JSON/index.js
+++ Day 16 JSON/index.js
@@ -21,12 +21,227 @@ const usersText = `{
     ]
     }`;
 
-let usersObj = JSON.parse(usersText, undefined, 4);
+let usersObj = JSON.parse(usersText);
 console.log(usersObj);
 
- usersObj = JSON.parse(usersText, (key, value) => {
+usersObj = JSON.parse(usersText, (key, value) => {
   let newValue =
     typeof value == "string" && key != "email" ? value.toUpperCase() : value;
   return newValue;
 });
 console.log(usersObj);
+console.clear();
+
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+let txt = JSON.stringify(users, undefined, 5);
+console.log(txt); // text means JSON- because json is a string form of an object.
+
+console.clear();
+
+/*Change skills array to JSON using JSON.stringify();
+  Stringify the age variable
+ Stringify the isMarried variable
+ Stringify the student object
+*/
+
+skills = ["HTML", "CSS", "JS", "React", "Node", "Python"];
+let age = 250;
+let isMarried = true;
+const student = {
+  firstName: "Asabeneh",
+  lastName: "Yetayehe",
+  age: 250,
+  isMarried: true,
+  skills: ["HTML", "CSS", "JS", "React", "Node", "Python"],
+};
+
+console.log(JSON.stringify(skills, undefined, 4));
+console.log(JSON.stringify(age));
+console.log(JSON.stringify(isMarried));
+console.log(JSON.stringify(student, undefined, 4));
+
+// Stringify the students object with only firstName, lastName and skills properties
+
+txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`;
+// Stringify the students object with only firstName, lastName and skills properties
+
+console.log(JSON.stringify(student, ["firstName", "lastName", "skills"], 4));
+
+console.clear();
+// Parse the txt JSON to object.
+// Find the user who has many skills from the variable stored in txt.
+
+let parsedJson = JSON.parse(txt, (key, value) => {
+  if (key == "skills") {
+    return value.length;
+  }
+  return value;
+});
+console.log(
+  Object.entries(parsedJson).sort((a, b) => {
+   return b[1].skills - a[1].skills;
+  })[0]
+);
