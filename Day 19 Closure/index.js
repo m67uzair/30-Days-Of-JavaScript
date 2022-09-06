fcfcdde9bbfe3ef69c89690d0198ce8d02de73e4
m67uzair <m67uzair@gmail.com>
2022-09-06 21:29:55 +0500

    excersices started

--- Day 19 Closure/index.js
+++ Day 19 Closure/index.js
@@ -1,14 +1,38 @@
+// function outerFunction() {
+//     let count = 0;
+//     function innerFunction() {
+//         count++
+//         return count
+//     }
+
+//     return innerFunction
+// }
+// const innerFunc = outerFunction()
+
+// console.log(innerFunc())
+// console.log(outerFunction())
+// console.log(innerFunc())
+
+console.clear();
 function outerFunction() {
     let count = 0;
-    function innerFunction() {
+    function plusOne() {
         count++
         return count
     }
+    function minusOne() {
+        count--
+        return count
+    }
 
-    return innerFunction
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
 }
-const innerFunc = outerFunction()
+const innerFuncs = outerFunction()
 
-console.log(innerFunc())
-console.log(innerFunc())
-console.log(innerFunc())
+console.log(outerFunction().plusOne)
+console.log(outerFunction().minusOne)
+console.log(innerFuncs.plusOne)
+console.clear();
