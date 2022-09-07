249d2d239da2023ae42f3ff3b2483d786a34523a
m67uzair <m67uzair@gmail.com>
2022-09-07 23:22:57 +0500

    started day 21

---
+++ Day 21 DOM-1/index.js
@@ -0,0 +1,21 @@
+
+let body = document.getElementsByTagName('body')[0];
+let year = document.getElementsByTagName('span')[0];
+let mainDiv = document.getElementsByClassName('wrapper')[0];
+let h2 = document.getElementsByTagName('h2')[0];
+
+body.style.fontFamily = "Roboto, sans-serif";
+// mainDiv.style.display = "flex"
+
+mainDiv.style.width = "max-content"
+mainDiv.style.margin = "0 auto"
+// mainDiv.style.flexDirection = "column"
+mainDiv.style.alig = "column"
+year.style.fontSize = "2em"
+year.style.fontWeight = "900"
+
+console.log();
+
+setInterval(() => {
+    
+}, 1000);
