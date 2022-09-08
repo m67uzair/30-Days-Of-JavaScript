e14d40f9ef55ac3c9275c5af4e4453840af21678
m67uzair <m67uzair@gmail.com>
2022-09-08 23:49:15 +0500

    pado

--- Day 21 DOM-1/index.js
+++ Day 21 DOM-1/index.js
@@ -1,21 +1,31 @@
+let body = document.getElementsByTagName("body")[0];
+let year = document.getElementsByTagName("span")[0];
+let mainDiv = document.getElementsByClassName("wrapper")[0];
+let h2 = document.getElementsByTagName("h2")[0];
+let date = new Date().toLocaleString("en-US", {
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  hour12: true,
+});
 
-let body = document.getElementsByTagName('body')[0];
-let year = document.getElementsByTagName('span')[0];
-let mainDiv = document.getElementsByClassName('wrapper')[0];
-let h2 = document.getElementsByTagName('h2')[0];
+date.toLocaleDateString;
 
 body.style.fontFamily = "Roboto, sans-serif";
-// mainDiv.style.display = "flex"
 
-mainDiv.style.width = "max-content"
-mainDiv.style.margin = "0 auto"
-// mainDiv.style.flexDirection = "column"
-mainDiv.style.alig = "column"
-year.style.fontSize = "2em"
-year.style.fontWeight = "900"
+mainDiv.style.width = "max-content";
+mainDiv.style.margin = "0 auto";
 
-console.log();
+h2.innerHTML = '<a href="https://www.pado.com">' + h2.innerHTML + "</a>";
+h2.style.fontSize = "16px";
+h2.style.textAlign = "center";
 
-setInterval(() => {
-    
-}, 1000);
+year.style.fontSize = "2em";
+year.style.fontWeight = "900";
+
+console.log(date);
+
+setInterval(() => {}, 1000);
