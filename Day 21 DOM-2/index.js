a341dd1b6eaa2dedf2ba26eab9ee507699336992
m67uzair <m67uzair@gmail.com>
2022-09-12 23:11:23 +0500

    ex level 1 done

--- Day 21 DOM-2/index.js
+++ Day 21 DOM-2/index.js
@@ -1,1 +1,22 @@
-//write something to just..... idk
+
+wrapperDiv = document.createElement('div');
+document.body.appendChild(wrapperDiv);
+wrapperDiv.style.maxWidth = '50%'
+wrapperDiv.style.height = '20%'
+wrapperDiv.style.display = 'flex'
+wrapperDiv.style.flexWrap = 'wrap'
+
+wrapperDiv.style.margin = '0 auto'
+
+for (let i = 0; i <= 101; i++) {
+  div = document.createElement("div");
+  div.style.width = '50px'
+  div.style.height = '50px'
+  div.style.backgroundColor = '#f64ee4'
+  div.style.margin = '1px'
+  div.style.textAlign = 'center'
+  div.style.lineHeight = '50px'
+
+  div.textContent = i ;
+  wrapperDiv.appendChild(div);
+}
