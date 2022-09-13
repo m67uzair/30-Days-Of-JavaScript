8ed9da279cd5bb7365414d02e99fea55ff21c862
m67uzair <m67uzair@gmail.com>
2022-09-13 23:15:47 +0500

    ex lvl 1 dine

--- Day 23 DOM-3 Event Listeners/index.js
+++ Day 23 DOM-3 Event Listeners/index.js
@@ -17,6 +17,9 @@
 //     e.target.textContent
 //   );
 // });
+
+let regex = /^\d+$/g;
+
 let input = document.querySelector("input");
 input.style.display = 'block'
 // input.style.marginLeft = '10px'
@@ -28,10 +31,13 @@ input.style.height = '30px'
 
 let button = document.querySelector('button')
 let value = -1;
+let pTag = document.querySelector('p');
+pTag.style.color = 'red';
+pTag.style.position = 'relative'; 
+pTag.style.width = '100%'; 
+pTag.style.left = '93px'; 
+pTag.style.fontSize = '14px';
 
-button.addEventListener('click',()=>{
-  value = input.value;
-});
 
 button.style.height = '35px'
 button.style.marginBottom = '15px'
@@ -45,15 +51,31 @@ wrapperDiv.style.flexWrap = "wrap";
 
 wrapperDiv.style.margin = "0 auto";
 
-for (let i = 0; i <= value; i++) {
-  div = document.createElement("div");
-  div.style.width = "50px";
-  div.style.height = "50px";
-  div.style.backgroundColor = "#f64ee4";
-  div.style.margin = "1px";
-  div.style.textAlign = "center";
-  div.style.lineHeight = "50px";
 
-  div.textContent = i;
-  wrapperDiv.appendChild(div);
-}
+button.addEventListener('click',()=>{
+  if (input.value == '') {
+    pTag.innerHTML = 'Please Enter A Valid Number Value.'
+  }
+  else if(input.value.match(regex) == null){
+    pTag.innerHTML = 'Input Value Must Be A Number'
+  }
+  value = input.value;
+
+  for (let i = 0; i <= value; i++) {
+    div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.backgroundColor = "#f64ee4";
+    div.style.margin = "1px";
+    div.style.textAlign = "center";
+    div.style.lineHeight = "50px";
+  
+    div.textContent = i;
+    wrapperDiv.appendChild(div);
+  }
+  
+
+});
+
+console.log(value);
+
